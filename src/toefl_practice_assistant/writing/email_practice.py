6e67bfff50f2ec43e098e46b0ee0a0f1908cf02f
alejandro negrete <alejandro.nr235@gmail.com>
2026-08-23 13:14:33 -0600

    feat: add PromptsConfigFrame

--- src/toefl_practice_assistant/writing/email_practice.py
+++ src/toefl_practice_assistant/writing/email_practice.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-from toefl_practice_assistant.core.gui_tools import LabeledEntry, LabeledCombobox
+from toefl_practice_assistant.core.gui_tools import LabeledCombobox, LabeledEntry
 
 
 class InstructionsFrame(ttk.Frame):
@@ -102,15 +102,74 @@ class ApiConfigFrame(ttk.Frame):
             self.api_entry.set_show(char="*")
 
 
+class PromptsConfigFrame(ttk.Frame):
+    """Custom widget to configure prompt related options, like user prompt for text generation and tags for talking with the assistant."""
+
+    def __init__(
+        self, parent: tk.Misc, default_prompt: str = "Generate a new email exercise."
+    ) -> None:
+        super().__init__(parent)
+        self.columnconfigure(index=0, weight=1)
+        self.rowconfigure(index=1, weight=1)
+
+        tags_frame = ttk.LabelFrame(self, text="Tags for talking with the assistant:")
+        tags_frame.columnconfigure(index=0, weight=1)
+        tags_frame.grid(row=0, column=0, sticky="we", padx=5, pady=5)
+
+        self.exercise_tag_entry = LabeledEntry(
+            parent=tags_frame,
+            label_text="Exercise tag",
+            label_width=10,
+            initial_entry_text="[EX]",
+        )
+        self.exercise_tag_entry.grid(row=0, column=0, sticky="we", padx=2, pady=5)
+
+        self.solution_tag_entry = LabeledEntry(
+            parent=tags_frame,
+            label_text="Solution tag",
+            label_width=10,
+            initial_entry_text="[SOL]",
+        )
+        self.solution_tag_entry.grid(row=1, column=0, sticky="we", padx=2, pady=5)
+
+        generation_prompts_frame = ttk.LabelFrame(
+            self, text="Prompt for exercise generation:"
+        )
+        generation_prompts_frame.rowconfigure(index=0, weight=1)
+        generation_prompts_frame.columnconfigure(index=0, weight=1)
+        generation_prompts_frame.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+
+        self.prompt_textbox = tk.Text(
+            generation_prompts_frame,
+            width=50,
+            height=5,
+            wrap="word",
+        )
+        self.prompt_textbox.grid(row=0, column=0, sticky="nswe", padx=5)
+        self.prompt_textbox.insert("1.0", default_prompt)
+
+    def get_exercise_generation_prompt(self) -> str:
+        return self.prompt_textbox.get("1.0", tk.END).strip()
+
+    def get_exercise_tag(self) -> str:
+        return self.exercise_tag_entry.get()
+
+    def get_solution_tag(self) -> str:
+        return self.solution_tag_entry.get()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
-    # root.rowconfigure(index=0, weight=1)
+    root.rowconfigure(index=0, weight=1)
     root.columnconfigure(index=0, weight=1)
 
     # instructions_frame = InstructionsFrame(root)
     # instructions_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-    api_config_frame = ApiConfigFrame(root, model_options=["Gemini", "GPT", "Claude"])
-    api_config_frame.grid(row=0, column=0, sticky="we", padx=5, pady=5)
+    # api_config_frame = ApiConfigFrame(root, model_options=["Gemini", "GPT", "Claude"])
+    # api_config_frame.grid(row=0, column=0, sticky="we", padx=5, pady=5)
+
+    prompts_config_frame = PromptsConfigFrame(root)
+    prompts_config_frame.grid(row=0, column=0, sticky="nswe", padx=5, pady=5)
 
     root.mainloop()
